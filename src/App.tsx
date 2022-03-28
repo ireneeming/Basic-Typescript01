5fe4ce9d9f71b07118b3f632ff7f919814cceade
ireneeming <minzigom@gmail.com>
2022-03-28 17:33:45 +0900

    🚧 : api 연결부분 작업중

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,28 @@ import { fetchCharacter } from './api';
 import { Character } from './api';
 
 function App() {
+  const [character, setCharacter] = React.useState({});
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [characterId, setCharacerId] = React.useState(1);
+
+  React.useEffect(() => {
+    const fetchFromApi = async () => {
+      setIsLoading(true);
+
+      const result = await fetchCharacter(characterId);
+      setIsLoading(false);
+      setCharacter(result);
+    };
+    fetchFromApi();
+  }, [characterId]);
+
   return (
     <TS.Wrapper>
-      <Card name="minji" imgUrl="https://finalspaceapi.com/img/gary_goodspeed.webp" />
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <Card name="minji" imgUrl="https://finalspaceapi.com/img/gary_goodspeed.webp" />
+      )}
     </TS.Wrapper>
   );
 }
